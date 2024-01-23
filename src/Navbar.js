b21c630e72a441ceb9173d1bb098dfd60e946159
enzosromano <enzosromano@gmail.com>
2024-01-23 16:12:08 -0500

    Update to active page links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,6 @@
-import { Link } from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function Navbar() {
-
-    const path = window.location.pathname
-
     return (
         <nav className="nav">
             <Link to="/" className="site-title">Site Name</Link>
@@ -16,11 +13,11 @@ export default function Navbar() {
 }
 
 function ActivePage({ to, children, ...props }) {
-
-    const path = window.location.pathname
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
 
     return (
-        <li className={path === to ? "active" : ""}>
+        <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>{children}</Link>
         </li>
     )
