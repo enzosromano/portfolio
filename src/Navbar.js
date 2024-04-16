ccb4335e83d051f573132ebef67c00cf5e2d16d7
enzosromano <enzosromano@gmail.com>
2024-04-15 21:16:53 -0400

    comment update

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,9 +12,11 @@ export default function Navbar() {
     )
 }
 
-function ActivePage({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+//Active pages intention is to set the active state to the selected link on the navbar
+
+function ActivePage({ to, children, ...props }) { // props ("properties") - stored as an object
+    const resolvedPath = useResolvedPath(to) // https://reactrouter.com/en/main/utils/resolve-path - resolves a given "To" value into an actual "Path" object
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
 
     return (
         <li className={isActive ? "active" : ""}>
