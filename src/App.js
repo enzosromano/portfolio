36c5cdcb6bfcdef83b0898177a5fdee1feb3197e
enzosromano <enzosromano@gmail.com>
2024-04-17 13:45:09 -0400

    project restructure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Blog from "./pages/Blog";
 import Links from "./pages/Links";
 import Projects from "./pages/Projects";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome Home!</h1>
+        <div className="about">
+            <h1>Enzo Romano</h1>
+            <h2>QA Analyst</h2>
+
+            <p>
+                QA Analyst and aspiring developer. Please feel free to reach me at 
+                any of my <a href="/links">links</a>, or by reaching out directly.
+            </p>
+            <h3>enzosromano@gmail.com</h3>
         </div>
     )
 }
