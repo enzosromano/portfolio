792f4ab234fc394c7ea7e9404597284fee3f935a
enzosromano <enzosromano@gmail.com>
2024-04-21 13:19:21 -0400

    navbar color change

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,11 @@
 export default function Projects() {
-    return <h1>Projects</h1>
+    return (
+        <div>
+            <h1>Projects</h1>
+
+            <p>I should make an app that shows the cons of not sanitizing input and the
+                consequences that could result in your DB.
+            </p>
+        </div>   
+)
 }
