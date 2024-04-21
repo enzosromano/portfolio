84547c9a8f6837e046151b5056240fed54ad9ec3
enzosromano <enzosromano@gmail.com>
2024-04-21 13:02:07 -0400

    added text to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,28 @@ export default function Links() {
     return (
         <div>
             <h1>About Me</h1>
+
+            <div className="summary">
+                <p>
+                    My name is Enzo Romano and I am currently a QA Analyst at Echelon Fitness.
+                </p>
+                <p>
+                    I love to build and break things which has led me to pursue a role as 
+                    either a QA Engineer or Full-Stack Developer.
+                    <br></br><br></br>
+                    I am constantly exploring new technologies and frameworks that catch
+                    my interest and finding ways to integrate them into existing applications
+                    along the way. Currently I am looking a testing framework called&nbsp;
+                        <a href="https://playwright.dev/" target="_blank" rel="noreferrer">Playwright</a>
+                    &nbsp;for end to end testing and expanding my experience in&nbsp;
+                        <a href="https://react.dev/" target="_blank" rel="noreferrer">React</a>
+                    &nbsp;to build interactive frontends.
+                </p>
+                <p>
+                    Please feel free to reach out with any questions or browse around my site to learn
+                    a bit more about me. 
+                </p>
+            </div>
             
         </div>
     )
