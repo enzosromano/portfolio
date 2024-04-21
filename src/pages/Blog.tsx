6cf7d7cd54e6d9147df5555922b5f2f905ff6554
enzosromano <enzosromano@gmail.com>
2024-04-21 13:21:17 -0400

    pulled out blog page code for fixing, cleaned up css comments

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,32 +2,7 @@ export default function Blog() {
     return (
         <div className="test1">
             <h1>This weeks entries...</h1>
-            
-            <div className="test2">
-                <h2>4/16/2024</h2>
-                <p>This is an example of a form being displayed</p>
-                <p>Look at all this information!</p>
-            </div>
-
-            <p>Todo</p>
-                <ul>
-                    <li>Decide on color pallete.</li>
-                    <li>Make blog components.</li>
-                    <li>Decide on hosting software.</li>
-                    <li>Set up DB.</li>
-                    <li>Make Mobile Friendly.</li>
-                    <li>Everything else.</li>
-            </ul>
-
-            <p>Todo</p>
-                <ul>
-                    <li>Decide on color pallete.</li>
-                    <li>Make blog components.</li>
-                    <li>Decide on hosting software.</li>
-                    <li>Set up DB.</li>
-                    <li>Make Mobile Friendly.</li>
-                    <li>Everything else.</li>
-            </ul>
+        
             
         </div>
     )
