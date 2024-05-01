b82275905cc994b43b6cb7eeed753b5b7555930c
enzosromano <enzosromano@gmail.com>
2024-05-01 13:31:27 -0400

    Update to projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
             <div className="projects">
                 <ProjectDisplay 
                     image="images/hackucf.png" 
-                    alt="test"
+                    alt="hack ucf logo"
                     title="CyberSecurity Competition Scoreboard"
                     text={<p>
                     Tasked with creating the scoreboard that the <b>Collegiate Cyber Defense Club @ UCF </b> 
@@ -37,12 +37,28 @@ export default function Projects() {
                 </ProjectDisplay>
 
                 <ProjectDisplay 
-                    image="images/construction.jpg" 
-                    alt="test"
-                    title="Site Under Construction"
-                    text="
-                    Working on it!
-                    ">
+                    image="images/QATool.jpg" 
+                    alt="image of QATool user interface"
+                    title="QATool"
+                    text={<p>
+                        Currently developing a Python application that can be downloaded and run by any QA member to automate daily tasks.
+                        Scripts are being developed with Python+Selenium and I added a simple GUI through&nbsp;
+                            <a href="https://docs.python.org/3/library/tkinter.html" target="_blank" rel="noreferrer">tkinter</a>
+                        &nbsp;since the application is not frontend focused.  
+                        <br></br><br></br>
+    
+                        The project is currently in its very early stages but is already capable of automating some basic tasks within
+                        the companies staging environment, reducing the time it takes to complete certain daily processes by 50% or more.
+                        I have plans to make a "public" version of this application with the GUI layout and in depth documentation to
+                        allow others to develop a similar tool, but without all my actual tests.
+    
+                        <br></br><br></br>
+                        <b>Why is this useful?</b> In my situation, we had a handful of daily tasks that needed to be automated, and there
+                        is not currently a test suite "management tool" that I have access to. If someone does not feel comfortable cloning a
+                        repo and running pytest manually through the command line, this allows them the full funcionality of the written automation
+                        tests with an easily digestible GUI to facilitate everything.
+                        </p>
+                        }>
                 </ProjectDisplay>
 
                 <ProjectDisplay 
